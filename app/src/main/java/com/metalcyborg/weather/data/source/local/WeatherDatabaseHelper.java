dea659696aabc8fb0b665c495f9cc4b6bf199d8a
Peter Trushkov <petertrushkov@gmail.com>
2017-05-06 22:59:54 +0500

    Added ChosenCities and Weather tables to DB

--- app/src/main/java/com/metalcyborg/weather/data/source/local/WeatherDatabaseHelper.java
+++ app/src/main/java/com/metalcyborg/weather/data/source/local/WeatherDatabaseHelper.java
@@ -30,6 +30,34 @@ public class WeatherDatabaseHelper extends SQLiteOpenHelper {
             WeatherPersistenceContract.FtsCityTable.COLUMN_LATITUDE + TYPE_FLOAT +
             " )";
 
+    private static final String CREATE_CHOSEN_CITIES_TABLE = "CREATE TABLE" +
+            WeatherPersistenceContract.ChosenCitiesTable.TABLE_NAME + " (" +
+            WeatherPersistenceContract.ChosenCitiesTable._ID + TYPE_TEXT + " PRIMARY KEY" + COMMA +
+            WeatherPersistenceContract.FtsCityTable.COLUMN_OPEN_WEATHER_ID + TYPE_TEXT + COMMA +
+            WeatherPersistenceContract.FtsCityTable.COLUMN_CITY_NAME + TYPE_TEXT + COMMA +
+            WeatherPersistenceContract.FtsCityTable.COLUMN_COUNTRY_NAME + TYPE_TEXT +
+            " )";
+
+    private static final String CREATE_WEATHER_TABLE = "CREATE TABLE" +
+            WeatherPersistenceContract.WeatherTable.TABLE_NAME + " (" +
+            WeatherPersistenceContract.WeatherTable._ID + TYPE_TEXT + " PRIMARY KEY" + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_DATE + TYPE_INTEGER + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_SUNRISE_TIME + TYPE_INTEGER + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_SUNSET_TIME + TYPE_INTEGER + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_TEMPERATURE + TYPE_FLOAT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_HUMIDITY + TYPE_FLOAT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_PRESSURE + TYPE_FLOAT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_WIND_SPEED + TYPE_FLOAT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_WIND_DIRECTION + TYPE_FLOAT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_CLOUDINESS + TYPE_INTEGER + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_CONDITION_ID + TYPE_INTEGER + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_WEATHER_GROUP + TYPE_TEXT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_DESCRIPTION + TYPE_TEXT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_ICON + TYPE_TEXT + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_VOLUME_RAIN_3H + TYPE_INTEGER + COMMA +
+            WeatherPersistenceContract.WeatherTable.COLUMN_VOLUME_SNOW_3H + TYPE_INTEGER +
+            " )";
+
     public WeatherDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
@@ -37,6 +65,8 @@ public class WeatherDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_FTS_CITY_TABLE);
+        db.execSQL(CREATE_CHOSEN_CITIES_TABLE);
+        db.execSQL(CREATE_WEATHER_TABLE);
     }
 
     @Override
--- app/src/main/java/com/metalcyborg/weather/data/source/local/WeatherPersistenceContract.java
+++ app/src/main/java/com/metalcyborg/weather/data/source/local/WeatherPersistenceContract.java
@@ -16,4 +16,31 @@ public class WeatherPersistenceContract {
         public static final String COLUMN_LONGITUDE = " Longitude";
         public static final String COLUMN_LATITUDE = " Latitude";
     }
+
+    public static class ChosenCitiesTable implements BaseColumns {
+        public static final String TABLE_NAME = " ChosenCities";
+        public static final String COLUMN_OPEN_WEATHER_ID = " OpenWeatherId";
+        public static final String COLUMN_CITY_NAME = " CityName";
+        public static final String COLUMN_COUNTRY_NAME = " CountryName";
+    }
+
+    public static class WeatherTable implements BaseColumns {
+        public static final String TABLE_NAME = " Weather";
+        public static final String COLUMN_CHOSEN_CITY_ID = " ChosenCityId";
+        public static final String COLUMN_DATE = " Date";
+        public static final String COLUMN_SUNRISE_TIME = " SunriseTime";
+        public static final String COLUMN_SUNSET_TIME = " SunsetTime";
+        public static final String COLUMN_TEMPERATURE = " Temperature";
+        public static final String COLUMN_HUMIDITY = " Humidity";
+        public static final String COLUMN_PRESSURE = " Pressure";
+        public static final String COLUMN_WIND_SPEED = " WindSpeed";
+        public static final String COLUMN_WIND_DIRECTION = " WindDirection";
+        public static final String COLUMN_CLOUDINESS = " Cloudiness";
+        public static final String COLUMN_CONDITION_ID = " ConditionId";
+        public static final String COLUMN_WEATHER_GROUP = " WeatherGroup";
+        public static final String COLUMN_DESCRIPTION = " Description";
+        public static final String COLUMN_ICON = " Icon";
+        public static final String COLUMN_VOLUME_RAIN_3H = " VolumeRain3h";
+        public static final String COLUMN_VOLUME_SNOW_3H = " VolumeSnow3h";
+    }
 }
